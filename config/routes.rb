cf6fae83768940c9ea71f174685ed5886172d92d
shi0n0 <shiono.yui1080@gmail.com>
2023-07-05 12:20:25 +0900

    各ページのルーティング及びdeviseのログアウトを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
+
+  get '/articles', to: 'articles#index'
+  get '/profiles', to: 'profiles#index'
+  get '/follows', to: 'follows#index'
+  get '/followers', to: 'followers#index'
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
